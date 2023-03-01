d09b0370ff8e0b9eefb14bfe70fac3191007ab2b
AM-SagarThakare <sagar.thakare@angularminds.in>
2023-03-01 14:05:23 +0530

    Array folder added

---
+++ Array/array1.js
@@ -0,0 +1,10 @@
+var arr = ["zero", "one", "two", "three"];
+
+console.log(arr[1]);
+console.log(arr['1']);
+
+console.log(arr[01]);
+console.log(arr["01"]);
+
+// console.log(arr[01]);            ith 0 picture mdhech yet nahi mhnun 1 pass hoto
+// console.log(arr['01']);          ith aapn direct string ch detoy 01 wali
---
+++ Array/array2.js
@@ -0,0 +1,7 @@
+var arr = [];
+
+arr[-1] = 10;
+arr[5]  = 10;
+arr[10]  = 10;
+
+console.log(arr);
